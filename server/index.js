952afcc6a9fc77c22cd2094149dcab39f49a8845
Aniket Sinha <aniketsinha161000@gmail.com>
2022-12-27 22:15:30 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 app.use("/posts", postRoutes);
+app.get("/", (req, res) => {
+  res.send("hello to memories API");
+});
 
 const PORT = process.env.PORT;
 const CONNECTION_URL = process.env.CONNECTION_URL;
